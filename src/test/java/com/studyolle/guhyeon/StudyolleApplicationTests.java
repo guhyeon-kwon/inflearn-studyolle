5a002b21131fa34ab55ac343d58189c264b8b060
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2021-11-12 16:57:45 +0900

    commit

---
+++ src/test/java/com/studyolle/guhyeon/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyolle.guhyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
